c6408139ece042b1fc405e0d2f4612d46311f900
Vitalii Shkolin <vitalii@shkolin.net.ua>
2026-02-05 12:24:36 +0200

    Create initial product models and migrations

--- 0-bs4/django_project/products/models.py
+++ 0-bs4/django_project/products/models.py
@@ -1,8 +1,9 @@
+import uuid
 from django.db import models
 
 
 class Product(models.Model):
-    id = models.UUIDField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField()
     color = models.CharField()
     ssd = models.CharField()
@@ -17,11 +18,42 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        db_table = 'products'
 
-class Image(models.Model):
-    id = models.UUIDField()
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+class ProductImage(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
     url = models.CharField()
 
     def __str__(self):
         return self.url
+
+    class Meta:
+        db_table = 'product_images'
+
+
+class CharacteristicGroup(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'characteristic_groups'
+
+
+class Characteristic(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    group = models.ForeignKey(CharacteristicGroup, related_name='characteristics', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='characteristics', on_delete=models.CASCADE)
+    name = models.CharField()
+    value = models.CharField()
+
+    def __str(self):
+        return '%s: %s' % (self.name, self.value)
+
+    class Meta:
+        db_table = 'characteristics'
